33603536749e4c0638f43ce7ccd51650ff17af21
Samuel Cruz <143637891+Samuelito1111@users.noreply.github.com>
2025-08-22 19:25:31 +0000

    feat: Aula de Condicionais

---
+++ aula 22-08/3. condicionais_aninhada.py
@@ -0,0 +1,15 @@
+# Condicionais Aninhada
+
+import os
+os.system("cls")
+
+idade = 20
+
+if idade >= 18:
+    print("Maior de idade!")
+elif idade >= 14:
+    print("Adolescente!")
+else:
+    print("Criança!")
+
+print("FIM")
---
+++ aula 22-08/4. maior_que.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+n1 = float(input("Digite um número: "))
+
+if n1 == 10:
+    print("O número é IGUAL a 10")
+elif n1 > 10:
+    print("O número é MAIOR que 10")
+else:
+    print("O número é MENOR que 10")
